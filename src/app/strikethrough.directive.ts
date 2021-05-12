4af9ce0f6bb4b26bbcab7554ef2e0e10546f5ef5
sngina <sngina707@gmail.com>
2021-05-12 14:31:54 +0300

    commit

---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,13 @@
+import { Directive,ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+   constructor(private elem:ElementRef){ }
+  
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+
+  }
+}
